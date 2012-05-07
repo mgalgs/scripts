f020a00599947667ecd0009a577e5cb1ba0b1909
Mitchel Humpherys <mitch.special@gmail.com>
2012-05-06 19:50:46 -0700

    aliases and rsync stuffs

--- dobackuptime.sh
+++ dobackuptime.sh
@@ -3,5 +3,5 @@
 # rsync -avu /media/space/Videos /home/mgalgs/
 # rsync -avu /home/mgalgs/Videos /media/space/
 
-rsync -rlvuz /media/space/Pictures /home/mgalgs/
-rsync -rlvuz /home/mgalgs/Pictures /media/space/
+rsync -rtlvuz /media/space/Pictures /home/mgalgs/
+rsync -rtlvuz /home/mgalgs/Pictures /media/space/
