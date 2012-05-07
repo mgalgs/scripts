5717705103fc17f2ab558842211091371b471707
Mitchel Humpherys <mitch.special@gmail.com>
2012-05-06 20:15:13 -0700

    improving phone syncing script

--- phonetime.sh
+++ phonetime.sh
@@ -2,6 +2,8 @@
 
 source ~/scripts/util.sh
 
+mountroot=/run/media/$(whoami)
+syncdest=/media/space
 printtag=' ::=> '
 
 declare -A phoneids
@@ -9,7 +11,7 @@ phoneids=([mitchel]=3AEB-1010 [sonnie]=BC4C-1008)
 whosephone=none
 
 for id in ${!phoneids[@]}; do
-	if [[ -d /media/${phoneids[$id]} ]]; then
+	if [[ -d ${mountroot}/${phoneids[$id]} ]]; then
 		whosephone=$id
 	fi
 done
@@ -25,13 +27,13 @@ mkdir -p ~/Phones
 
 # Grab the stuff from the phone
 bold_print $printtag "Syncing phone to local Phones folder"
-rsync -avuz --exclude '.thumbnails' /media/${phoneids[$whosephone]}/DCIM /home/mgalgs/Phones/$whosephone
+rsync -avuz --exclude '.thumbnails' ${mountroot}/${phoneids[$whosephone]}/DCIM /home/mgalgs/Phones/$whosephone
 
 # sync to sonch if it's up:
-if grep -qs /media/space /proc/mounts; then
-	bold_print $printtag "Syncing local Phones folder to "
-	rsync -avuz --no-g /home/mgalgs/Phones /media/space/
-	rsync -avuz --no-g /media/space/Phones /home/mgalgs/
+if grep -qs $syncdest /proc/mounts; then
+	bold_print $printtag "Syncing local Phones folder to sonch"
+	rsync -avuz --no-g /home/mgalgs/Phones $syncdest
+	rsync -avuz --no-g ${syncdest}/Phones /home/mgalgs/
 else
 	bold_print $printtag "Couldn't sync to space (is the freenas server accessible?)"
 fi
