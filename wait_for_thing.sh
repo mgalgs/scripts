44c45365e6598db166d0bcbd1c08c3a9690afd2c
Mitchel Humpherys <mitch.special@gmail.com>
2012-07-17 19:58:21 -0700

    wait for thing script

---
+++ wait_for_thing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# You might also want to add this to your .bashrc:
+# complete -F _pgrep wait_for_thing.sh
+
+
+[[ $# -ne 1 || $1 == "-h" || $1 == "--help" ]] && { echo "usage: $0 name"; exit 1; }
+
+pgrep $1 || { echo "nothing much"; exit 0; }
+echo "waiting for $1 ($(pgrep $1 | tr '\n' ','))"
+while pgrep $1 2>&1 >/dev/null; do
+    echo -n '.'
+    sleep 1
+done
+echo
+xmessage -nearmouse "wait_for_thing \`$1' is complete"
