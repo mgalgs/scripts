b47807d5c59032ca5e5cf552ede8aba6cea8338b
Mitchel Humpherys <mitch.special@gmail.com>
2012-08-26 23:29:09 -0700

    use pidof instead of pgrep for robustness

--- wait_for_thing.sh
+++ wait_for_thing.sh
@@ -6,11 +6,12 @@
 
 [[ $# -ne 1 || $1 == "-h" || $1 == "--help" ]] && { echo "usage: $0 name"; exit 1; }
 
-pgrep $1 || { echo "nothing much"; exit 0; }
-echo "waiting for $1 ($(pgrep $1 | tr '\n' ','))"
-while pgrep $1 2>&1 >/dev/null; do
+pidof $1 || { echo "nothing much"; exit 0; }
+echo "waiting for $1 ($(pidof $1 | tr '\n' ','))"
+while pidof $1 2>&1 >/dev/null; do
     echo -n '.'
     sleep 1
 done
+
 echo
 xmessage -nearmouse "wait_for_thing \`$1' is complete"
