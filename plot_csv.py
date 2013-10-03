35856592708b3dedf2a3d0829851e4238996d2bf
Mitchel Humpherys <mitch.special@gmail.com>
2013-10-03 13:24:54 -0700

    plot_csv.py: add command line options for y-axis limits

--- plot_csv.py
+++ plot_csv.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python2
 
 import sys
+import os
 import csv
+import argparse
 from itertools import cycle
 from matplotlib import pyplot as plt
 
 
+DESCRIPTION = """Plots csv data from sdtin. Data should be in the following format:
+
+header1,header2,...
+data,data,...
+data,data,...
+"""
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('title')
+    parser.add_argument('ylabel')
+    parser.add_argument('xlabel')
+    parser.add_argument('--y-bottom', type=int, metavar='YB',
+                        help='Lower limit for y-axis on plot')
+    parser.add_argument('--y-top', type=int, metavar='YT',
+                        help='Upper limit for y-axis on plot')
+
+    args = parser.parse_args()
+
     reader = csv.reader(sys.stdin)
     lines = []
     for row in reader:
@@ -23,11 +45,16 @@ if __name__ == "__main__":
     ymin -= .1 * yrange
     ymax += .1 * yrange
 
+    if args.y_bottom is not None:
+        ymin = args.y_bottom
+    if args.y_top is not None:
+        ymax = args.y_top
+
     p1 = plt.plot(data, '-o')
     plt.legend(p1, titles)
     plt.ylim([ymin, ymax])
-    plt.title(sys.argv[1])
-    plt.ylabel(sys.argv[2])
-    plt.xlabel(sys.argv[3])
+    plt.title(args.title)
+    plt.ylabel(args.ylabel)
+    plt.xlabel(args.xlabel)
     plt.grid(True)
     plt.show()
