df54ab17493459f03c87e01d5035cd68282c53e9
Mitchel Humpherys <mitch.special@gmail.com>
2014-04-07 10:24:27 -0700

    plot_csv.py: initial scatterplot support

--- plot_csv.py
+++ plot_csv.py
@@ -6,6 +6,7 @@ import csv
 import argparse
 from itertools import cycle
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 DESCRIPTION = """Plots csv data from sdtin. Data should be in the following format:
@@ -26,8 +27,16 @@ if __name__ == "__main__":
                         help='Lower limit for y-axis on plot')
     parser.add_argument('--y-top', type=float, metavar='YT',
                         help='Upper limit for y-axis on plot')
+    parser.add_argument('--x-bottom', type=float, metavar='XB',
+                        help='Lower limit for x-axis on plot')
+    parser.add_argument('--x-top', type=float, metavar='XT',
+                        help='Upper limit for x-axis on plot')
+    parser.add_argument('--scatter', action='store_true',
+                        help='Generate a scatter plot. When given, there should only be two columns of data.')
 
     args = parser.parse_args()
+
+    xlabel = ylabel = None
 
     reader = csv.reader(sys.stdin)
     lines = []
@@ -57,14 +66,32 @@ if __name__ == "__main__":
     if args.y_top is None:
         ymax += .1 * yrange
 
-    p1 = plt.plot(data, '-o')
-    plt.legend(p1, titles, loc='best')
+    xmin = args.x_bottom
+    xmax = args.x_top
+
+    if args.scatter:
+        data = np.array(data)
+        plt.scatter(data[:, 0], data[:, 1])
+        if xmin is None:
+            xmin = min(data[:, 0])
+        if xmax is None:
+            xmax = xmax or max(data[:, 0]) + 1
+        xlabel, ylabel = titles
+    else:
+        p1 = plt.plot(data, '-o')
+        plt.legend(p1, titles, loc='best')
+
+    if xmin is not None and xmax is not None:
+        plt.xlim([xmin, xmax])
+
     plt.ylim([ymin, ymax])
+
     if args.title:
         plt.title(args.title.replace('\\n', '\n'))
-    if args.ylabel:
-        plt.ylabel(args.ylabel)
-    if args.xlabel:
-        plt.xlabel(args.xlabel)
+    if args.ylabel or ylabel:
+        plt.ylabel(args.ylabel or ylabel)
+    if args.xlabel or xlabel:
+        plt.xlabel(args.xlabel or xlabel)
+
     plt.grid(True)
     plt.show()
