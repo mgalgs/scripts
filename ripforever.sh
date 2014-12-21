4f423e8732709141dae43d013e1656b53eb5d227
Mitchel Humpherys <mitch.special@gmail.com>
2014-12-20 22:55:29 -0800

    ripforever: resiliency

--- ripforever.sh
+++ ripforever.sh
@@ -15,13 +15,31 @@ waitfordisc()
         echo -n .
         sleep 5
     done
+    # bonus mount seems to help get things going... don't ask me...
+    mkdir -p /mnt/ripforever
+    mount $DVDDEVICE /mnt/ripforever
+    umount /mnt/ripforever
+    rm -r /mnt/ripforever
+}
+
+errorout()
+{
+    eject $DVDDEVICE
+    exit 1
 }
 
 while :; do
     waitfordisc
-    isoname=$(echo "$(blkid $DVDDEVICE -o value -s LABEL).iso")
+    isobase=$(blkid $DVDDEVICE -o value -s LABEL)
+    if [[ -z "$isobase" ]]; then
+	# couldn't get disc title with blkid :( let's try lsdvd.
+	echo "Couldn't get disc title with blkid... Trying lsdvd..."
+	isobase=$(lsdvd $DVDDEVICE | grep "Disc Title:" | cut -d: -f2 | cut -c2-)
+    fi
+    [[ -z "$isobase" ]] && { echo "Couldn't get disc title. bailing."; errorout; }
+    isoname="${isobase}.iso"
     echo "ripping $isoname"
-    dd if=$DVDDEVICE of=$OUTDIR/$isoname || { echo "dd failed. bailing."; exit 1; }
+    dd if=$DVDDEVICE of=$OUTDIR/$isoname bs=8k || { echo "dd failed. bailing."; errorout; }
     chown $ISOOWNER:$ISOGROUP $OUTDIR/$isoname
     echo 'done!'
     eject $DVDDEVICE
