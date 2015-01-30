53b5389d359c02db1b8f9d0ba7d8f61799ab1154
Mitchel Humpherys <mitch.special@gmail.com>
2015-01-29 22:01:42 -0800

    dd-one: quicker ddrescue options

--- dd-one.sh
+++ dd-one.sh
@@ -81,19 +81,17 @@ done
 [[ $success = yes ]] || {
     log "dd failed. making one last attempt with ddrescue..."
 
-    # for all: 3 retries, block size=2048 (which is what the manual
-    # suggests for cdroms)
+    # for all: block size=2048 (which is what the manual suggests for
+    # cdroms)
 
     # need a loop for the `break'...
     while :; do
-        log "first trying with no scraping."
-        ddrescue -n -r 3 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
-        log "no dice.  Now trying direct access."
-        ddrescue -d -r 3 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
+        log "first trying with direct access"
+        ddrescue -d -r 1 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
         log "still no dice. Trying with retrim."
-        ddrescue -d -R -r 3 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
+        ddrescue -d -R -r 1 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
         log "still no... Just try one more time..."
-        ddrescue    -r 3 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
+        ddrescue    -r 1 -b2048 $DEVNAME $IMGNAME $DDRESCUE_LOGFILE &>$DDRESCUE_OUTPUT_LOGFILE && break
         log "Nothing worked. Relenting..."
         errorout
     done
