e41e4701f7f8502737160520a79e5001fa030abc
Mitchel Humpherys <mitch.special@gmail.com>
2015-07-28 23:05:49 -0700

    plot_csv.py: add --smooth option

--- plot_csv.py
+++ plot_csv.py
@@ -33,6 +33,11 @@ if __name__ == "__main__":
                         help='Upper limit for x-axis on plot')
     parser.add_argument('--scatter', action='store_true',
                         help='Generate a scatter plot. When given, there should only be two columns of data.')
+    parser.add_argument('--smooth', type=int,
+                        nargs='?', const=11, metavar='window_len',
+                        help='Smooth the data by convolving it with a Hanning window of the given length. window_len defaults to 11.')
+    parser.add_argument('--raw', action='store_true',
+                        help='When --smooth is given, raw data is not plotted. This overrides that behavior, forcing raw data to be plotted as well.')
 
     args = parser.parse_args()
 
@@ -52,8 +57,39 @@ if __name__ == "__main__":
             print 'Skipping invalid line (%d):' % cnt, line
             pass
 
-    ymin = min([min(row) for row in data])
-    ymax = max([max(row) for row in data])
+    data = np.array(data)
+
+    xmin = args.x_bottom
+    xmax = args.x_top
+
+    if args.scatter:
+        plt.scatter(data[:, 0], data[:, 1])
+        if xmin is None:
+            xmin = min(data[:, 0])
+        if xmax is None:
+            xmax = xmax or max(data[:, 0]) + 1
+        xlabel, ylabel = titles
+    else:
+        smoothed = []
+        linestyle = os.getenv('LINESTYLE', '-o')
+        if args.smooth:
+            w = np.hamming(args.smooth)
+            smoothed = []
+            for d in data.transpose():
+                out = np.convolve(d, w / w.sum(), mode='valid')
+                smoothed.append(out)
+            smoothed = np.array(smoothed).transpose()
+            psmoothed = plt.plot(smoothed,  linestyle)
+            # plt.legend(psmoothed, 'Smoothed')
+        if not args.smooth or args.raw:
+            p1 = plt.plot(data, linestyle)
+            plt.legend(p1, titles, loc='best')
+
+    if xmin is not None and xmax is not None:
+        plt.xlim([xmin, xmax])
+
+    ymin = data.min()
+    ymax = data.max()
 
     if args.y_bottom is not None:
         ymin = args.y_bottom
@@ -65,24 +101,6 @@ if __name__ == "__main__":
         ymin -= .1 * yrange
     if args.y_top is None:
         ymax += .1 * yrange
-
-    xmin = args.x_bottom
-    xmax = args.x_top
-
-    if args.scatter:
-        data = np.array(data)
-        plt.scatter(data[:, 0], data[:, 1])
-        if xmin is None:
-            xmin = min(data[:, 0])
-        if xmax is None:
-            xmax = xmax or max(data[:, 0]) + 1
-        xlabel, ylabel = titles
-    else:
-        p1 = plt.plot(data, os.getenv('LINESTYLE', '-o'))
-        plt.legend(p1, titles, loc='best')
-
-    if xmin is not None and xmax is not None:
-        plt.xlim([xmin, xmax])
 
     plt.ylim([ymin, ymax])
 
